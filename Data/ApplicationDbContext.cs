7169b07848bf90e9d2037a2466d83f6911caf8a9
YanpieroCutipa <yanpiero_cutipa@usmp.pe>
2024-04-22 09:03:57 -0500

    Catalogo y Producto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,10 +6,9 @@ namespace zefffood.Data;
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-        
-    }
+        : base(options){     
+        }
     public DbSet<zefffood.Models.Contacto> DataContacto {get; set; }
+    
     public DbSet<zefffood.Models.Producto> DataProducto {get; set; }
 }
